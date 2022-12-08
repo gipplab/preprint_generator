220d7c949ca9d1c1fdb6fb0de03fd024b473bcda
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-12-08 20:48:02 +0100

    feat: added routes to access and write to the database

---
+++ backend/routes/database.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var {insertPreprint, getSimilarPreprints} = require("../database/database")
+
+router.put("/storePreprint", function (req, res, next) {
+    insertPreprint(req.query.title, req.query.doi, JSON.parse(req.query.keywords).map((keyword) => keyword.toLowerCase()));
+    res.send(req.query);
+});
+
+router.get('/getRelatedPreprints', async function (req, res, next) {
+    res.send(JSON.stringify(await getSimilarPreprints(JSON.parse(req.query.keywords).map((keyword) => keyword.toLowerCase()))));
+});
+
+module.exports = router;
