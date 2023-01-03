557a8540d9ab29becb7e5435f52236e83130dece
lkaesberg <larskaesberg@yahoo.de>
2023-01-03 20:28:14 +0100

    feat: changed title position in appbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {PDFDocument} from 'pdf-lib'
 import {PDFFileForm} from "./PDFFileForm";
 import {parsePDF, PDFFile, PDFInfo} from "./pdf/PDFParser";
 import {createBibTexAnnotation} from "./pdf/PDFBibTexAnnotationGenerator";
-import {AppBar, Box, Button, IconButton, Toolbar, Tooltip, Typography} from "@mui/material";
+import {AppBar, Box, Button, Grid, Icon, IconButton, Toolbar, Tooltip, Typography} from "@mui/material";
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import ArticleIcon from '@mui/icons-material/Article';
 import {TagInputField} from "./TagInputField";
@@ -57,26 +57,28 @@ class App extends Component<AppProps, AppState> {
         return (
             <ThemeProvider theme={darkTheme}>
                 <div className="App">
-                    <Box sx={{flexGrow: 1}}>
-                        <AppBar position="static">
-                            <Toolbar>
-                                <ArticleIcon/>
-                                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                    <AppBar position="static">
+                        <Toolbar style={{justifyContent: "space-between", alignItems: "center"}}>
+                            <div style={{display: "flex", alignItems: "center"}}>
+                                <ArticleIcon sx={{mr: 2}}/>
+                                <Typography variant="h5" component="div" sx={{flexGrow: 1}}>
                                     {this.state.file ? this.state.file.name : "Enhanced Preprint Generator"}
                                 </Typography>
+                            </div>
+                            <div style={{display: "flex", alignItems: "center"}}>
                                 <Button color="inherit" onClick={() => {
                                     this.setState({file: undefined})
                                 }}>Reset Document</Button>
                                 {this.state.apiConnected ? (
                                     <Tooltip title="API connected!"><PowerIcon/></Tooltip>) : (
                                     <Tooltip title="API disconnected!"><PowerOffIcon/></Tooltip>)}
-                            </Toolbar>
-                        </AppBar>
-                    </Box>
+                            </div>
+                        </Toolbar>
+                    </AppBar>
                     <header className="App-header">
                         {(!this.state.file) &&
                             <>
-                                <h5>Drag & Drop a preprint PDF to enhance it! </h5>
+                                <h6>Drag & Drop a preprint PDF to enhance it! </h6>
                                 <FileUploader handleChange={async (file: any) => {
                                     let pdfDoc = await PDFDocument.load(await toBase64(file))
                                     let pdfFile: PDFFile = {
@@ -93,7 +95,7 @@ class App extends Component<AppProps, AppState> {
                             </>}
                         {this.state.file &&
                             <div style={{maxWidth: "80%"}}>
-                                <h5>BibTex Information</h5>
+                                <h6>BibTex Information</h6>
                                 <PDFFileForm onSubmit={async (bibTexEntries) => {
                                     await createBibTexAnnotation(
                                         this.state.file!.file,
@@ -102,7 +104,7 @@ class App extends Component<AppProps, AppState> {
                                     )
                                 }} info={this.state.file.info}/>
                                 <br/>
-                                <h5>Keywords</h5>
+                                <h6>Keywords</h6>
                                 <div style={{position: "relative", bottom: 0, left: 0}}>
                                     <TagInputField/>
                                 </div>
