c3fa9ed3f5f201c084dbac675b0ce0193ed46c6f
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2023-01-27 01:37:20 +0100

    fix: changed path to config

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
 var databaseRouter = require("./routes/database");
-var config = require("config.json")
+var config = require("./config.json")
 
 var cors = require("cors");
 
