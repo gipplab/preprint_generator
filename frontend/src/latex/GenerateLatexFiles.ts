e1a7023396048782c3d0de49c91f66b58d7d61e9
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-07-02 17:25:20 +0200

    feat: bundle latex files as zip

--- frontend/src/latex/GenerateLatexFiles.ts
+++ frontend/src/latex/GenerateLatexFiles.ts
@@ -1,9 +1,11 @@
 import {latexSty} from "./LatexSty";
 import {latexMD} from "./LatexMD";
 import {generateLatexTex} from "./LatexTex";
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function downloadFileFromText(content: string, filename: string): void {
-    const blob = new Blob([content], {type: 'text/plain;charset=utf-8'});
+    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
     const downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.download = filename;
@@ -12,13 +14,18 @@ function downloadFileFromText(content: string, filename: string): void {
     document.body.removeChild(downloadLink);
 }
 
-// Example usage
 export function downloadLatexFiles(annotation: string, link: string, relatedPapers: string[], onlineLink: boolean = true): void {
-    const styText = latexSty
-    const texText = generateLatexTex(annotation, link, relatedPapers, onlineLink)
-    const mdText = latexMD
+    const styText = latexSty;
+    const texText = generateLatexTex(annotation, link, relatedPapers, onlineLink);
+    const mdText = latexMD;
 
-    downloadFileFromText(styText, 'annotation.sty');
-    downloadFileFromText(texText, 'annotation.tex');
-    downloadFileFromText(mdText, 'instructions.md');
+    const zip = new JSZip();
+    zip.file('annotation.sty', styText);
+    zip.file('annotation.tex', texText);
+    zip.file('instructions.md', mdText);
+
+    zip.generateAsync({ type: 'blob' })
+        .then(function (blob) {
+            saveAs(blob, 'latex_files.zip');
+        });
 }
